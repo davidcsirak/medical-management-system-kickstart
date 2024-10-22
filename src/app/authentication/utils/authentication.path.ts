884b476edefcf1b7d9c83d00947ae1978779f09e
Dávid Csirák <david.csirak@innovitech.hu>
2024-10-22 14:42:37 +0200

    new ip config change

--- src/app/authentication/utils/authentication.path.ts
+++ src/app/authentication/utils/authentication.path.ts
@@ -1,4 +1,4 @@
-const BASE_API_URL = 'http://84.236.29.133:8080/api/v1';
+const BASE_API_URL = 'http://85.238.76.216:8080/api/v1';
 export const AUTHENTICATE_URL = `${BASE_API_URL}/auth/authenticate`;
 export const CURRENT_USER_URL = `${BASE_API_URL}/me`;
 export const REFRESH_URL = `${BASE_API_URL}/auth/refresh`;
